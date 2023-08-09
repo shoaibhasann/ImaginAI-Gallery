f71e3a53a116ba64e0d8128ade053abc058b738b
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-10 00:48:08 +0530

    change logic for download image

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import {surpriseMePrompts} from '../constants';
+import FileSaver from 'file-saver';
 
 
 export function getRandomPrompt(prompt) {
@@ -9,4 +10,8 @@ export function getRandomPrompt(prompt) {
         return getRandomPrompt(prompt);
     }
     return randomPrompt
+}
+
+export function downloadImage(_id,photo){
+    FileSaver.saveAs(photo, `ImaginAi-Gallery-${_id}.png`);
 }
